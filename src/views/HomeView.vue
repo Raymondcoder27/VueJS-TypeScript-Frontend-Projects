44d65363cf6cf4eeb719b53775432fe0223a8e84
MwebeCoder27 <mwebraymond13@gmail.gom>
2024-08-16 19:17:58 +0300

    sets up project, tailwind, and styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,25 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'App',
+    components: {},
+    data() {
+      return {
+        name: 'Link'
+      }
+    },
+    methods: {
+      changeName(name: string){
+        this.name = name;
+      }
+    }
+  })
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h2>Hello {{name}}</h2><h2>Welcome to Dev Jobs</h2>
+    <button @click="changeName('Raymond')">Change Name</button>
   </main>
 </template>
