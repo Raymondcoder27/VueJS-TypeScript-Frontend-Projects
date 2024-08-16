2eda4e01d2955db681f2c7fb749f2236f4deb0d0
MwebeCoder27 <mwebraymond13@gmail.gom>
2024-08-16 19:49:27 +0300

    sets up more resources

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,8 @@ import { defineComponent } from 'vue';
     components: {},
     data() {
       return {
-        name: 'Link'
+        name: 'Link',
+        age: 34 as number | string
       }
     },
     methods: {
@@ -18,8 +19,13 @@ import { defineComponent } from 'vue';
 </script>
 
 <template>
-  <main>
+  <main class="center">
     <h2>Hello {{name}}</h2><h2>Welcome to Dev Jobs</h2>
-    <button @click="changeName('Raymond')">Change Name</button>
+    <button @click="changeName('Raymond')" class="button">Change Name</button>
   </main>
 </template>
+
+
+<style scoped>
+  @import "@/assets/styles/buttons.css";
+</style>
